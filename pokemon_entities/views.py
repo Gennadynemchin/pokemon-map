385e80d9e287cb92d5b02ed02bb3af01a6640afd
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-03-13 13:59:15 +0300

    show pokemons on mainpage;

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,6 @@
 import folium
 import json
-
+from pokemon_entities.models import Pokemon, PokemonEntity
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
@@ -40,11 +40,12 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    pokemons_from_db = Pokemon.objects.all()
+    for pokemon in pokemons_from_db:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(f'/media/{pokemon.image}'),
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
